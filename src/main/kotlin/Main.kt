b4c54fc141aa9dbcaf8344da44eff59ca58e1ab7
Risda Tamam <risda123tamam@gmail.com>
2021-09-09 04:06:14 +0700

    penerapan Getter Setter, Constructor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,28 @@
-fun Car(_carName: String, _typeCar: String) {
+// Constructor in Car class
+class Car(_carName: String, _typeCar: String) {
     var carName = _carName.capitalize()
     var typeCar = _typeCar
 
-//    init {
-//        println("Nama Mobil = $carName")
-//        println("Type Mobil = $typeCar")
-//    }
+    // Getter and Setter
+    get() = field
+    set(value) {
+        field = value
+    }
+
+    // Initializer Block
+    init {
+        println("Nama Mobil = $carName")
+        println("Type Mobil = $typeCar")
+    }
 }
 
 fun main() {
     // List Car
-    val carList = arrayListOf("BMW", "Mercedes Benz", "Lamborgini", "Ferrari", "Mazda")
+    val carNamelist = arrayListOf("BMW", "Mercedes Benz", "Lamborgini", "Ferrari", "Mazda")
+    val carTypelist = arrayListOf("Sport", "Classic")
+
+    // Memanggil constructor pada class Car
+    println("---- Deskripsi Mobil ----")
+    val car1 = Car(carNamelist[0], carTypelist[0])
+    println("-------------------------")
 }
