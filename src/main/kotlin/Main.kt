55b9d87e523a806d08529818cd6249c8b998f85c
Risda Tamam <risda123tamam@gmail.com>
2021-09-09 03:41:43 +0700

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun Car(_carName: String, _typeCar: String) {
+    var carName = _carName.capitalize()
+    var typeCar = _typeCar
+
+//    init {
+//        println("Nama Mobil = $carName")
+//        println("Type Mobil = $typeCar")
+//    }
+}
+
+fun main() {
+    // List Car
+    val carList = arrayListOf("BMW", "Mercedes Benz", "Lamborgini", "Ferrari", "Mazda")
+}
